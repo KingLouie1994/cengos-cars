75082749d4d4d4c912003ec54bc4703729b46b64
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-03-11 23:05:13 +0100

    first draft of a image carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
+      <Carousel />
     </div>
   );
 }
