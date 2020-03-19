171376765ca7879677793d4194e110f52404d2c7
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-03-19 10:35:23 +0100

    draft of home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import Carousel from "./components/Carousel/Carousel";
 import Footer from "./components/Footer/Footer";
+import Homepage from "./components/Homepage";
+import Cars from "./components/Cars";
+import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Navbar />
-      <Carousel />
-      <Footer />
+    <div className={styles.pageContainer}>
+      <div className={styles.contentWrap}>
+        <Header />
+        <Navbar />
+        <Route exact path="/" component={Homepage} />
+        <Route path="/caroffers" component={Cars} />
+        <Route path="/services" component={Services} />
+        <Route path="/about" component={AboutUs} />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContentHome from "./ContentHome/ContentHome";
+import Carousel from "./Carousel/Carousel";
+
+const Homepage = () => {
+  return (
+    <>
+      <ContentHome />
+      <Carousel />
+    </>
+  );
+};
+
+export default Homepage;
