fbd010467fe7f8053cb9fcf813380afc0637d0e8
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-03-23 14:25:12 +0100

    lowest to highest and the other way round

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -6,7 +6,6 @@ import Filter from "../Filter/Filter";
 const Products = () => {
   const [cars, setCars] = useState([]);
   const [sort, setSort] = useState("");
-  const [manufacturer, setManufacturer] = useState("");
 
   useEffect(() => {
     axios
@@ -22,25 +21,52 @@ const Products = () => {
       });
   }, []);
 
+  const lowest = (a, b) => {
+    if (a.Price < b.Price) {
+      return -1;
+    }
+    if (a.Price > b.Price) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const highest = (a, b) => {
+    if (a.Price > b.Price) {
+      return -1;
+    }
+    if (a.Price < b.Price) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const sortId = (a, b) => {
+    if (a.id < b.id) {
+      return -1;
+    }
+    if (a.id > b.id) {
+      return 1;
+    }
+    return 0;
+  };
+
   const handleSortChange = e => {
     setSort(e.target.value);
+    if (e.target.value === "lowestprice") {
+      cars.sort(lowest);
+    } else if (e.target.value === "highestprice") {
+      cars.sort(highest);
+    } else {
+      cars.sort(sortId);
+    }
   };
-
-  const handleManufacturerChange = e => {
-    setManufacturer(e.target.value);
-  };
-
-  console.log(sort);
-  console.log(manufacturer);
 
   return (
     <>
       <div className={styles.page}>
         <h1>Unser regelmäßig wechselndes Fahrzeugangebot</h1>
-        <Filter
-          handleSortChange={handleSortChange}
-          handleManufacturerChange={handleManufacturerChange}
-        />
+        <Filter handleSortChange={handleSortChange} />
         <div className={styles.container}>
           {cars.map(car => {
             return (
